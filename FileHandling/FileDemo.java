683347d1cc08370e4a2c8184b94db82b5fabdcd4
sakshi-0009 <sakshigome1809@gmail.com>
2023-09-12 14:55:32 +0530

    File Handling

--- FileHandling/FileDemo.java
+++ FileHandling/FileDemo.java
@@ -26,9 +26,30 @@ public class FileDemo {
 			}
 		}
 		System.out.println(count);
+		
+		System.out.println(fobj1.getName()); 		//FileHandingDemo
+		System.out.println(fobj2.getName()); 		//Code.txt
 
-		System.out.println(fobj1.canExecute());
-		System.out.println(fobj1.canRead());
-		System.out.println(fobj1.canWrite());
+		System.out.println(fobj1.getParent()); 		//null
+		System.out.println(fobj2.getName()); 		//code.txt
+		
+		System.out.println(fobj1.getPath()); 		//FilehandlingDemo
+		System.out.println(fobj2.getPath()); 		//Code.txt
+		
+		System.out.println(fobj2.getAbsolutePath()); //C:\Users\User\eclipse-workspace\03_PracticeCodes\Code.txt
+		System.out.println(fobj2.getCanonicalPath()); //C:\Users\User\eclipse-workspace\03_PracticeCodes\Code.txt
+		
+		System.out.println(fobj2.canExecute());		//true
+		System.out.println(fobj2.canRead());		//true
+		System.out.println(fobj2.canWrite());		//true
+		
+		System.out.println(fobj2.isDirectory()); 	//false
+		System.out.println(fobj2.isFile()); 		//true
+		System.out.println(fobj2.isHidden()); 		//false
+		
+		System.out.println(fobj2.lastModified()); 	//1694430787287
+		System.out.println(fobj2.length());  		//18
+		System.out.println(fobj2.compareTo(fobj1)); //-3
+
 	}
 }
